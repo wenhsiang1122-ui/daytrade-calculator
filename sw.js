f3dc33b1d1f6b22cfa19c3e08d8b192d0167a64a
wenhsiang1122-ui <wenhsiang1122@gmail.com>
2026-08-13 15:24:32 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const C="daytrade-calculator-v1-5";const A=["./","./index.html","./manifest.webmanifest","./icon.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
